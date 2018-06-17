7026cdb1742e531f05697018ade936a4ff7b1db9
Yev Barkalov <yevgeniybarkalov@yahoo.com>
2018-06-17 00:18:54 -0400

    Remove python files

--- script.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from os import system
-
-while True:
-    sleep(3)
-    
-    system(
-        """osascript -e ‘tell application “Finder” to set desktop picture to POSIX file “~/114.jpg”’"""
-        )

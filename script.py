5165070fe07b5d9ac85db1cabb9cdc4b4173afcb
Yev Barkalov <yevgeniybarkalov@yahoo.com>
2018-06-16 23:50:50 -0400

    Rename files

---
+++ script.py
@@ -0,0 +1,6 @@
+from time import sleep
+from os import system
+
+while True:
+    sleep(3)
+    system("osascript -e \‘tell application \“Finder\” to set desktop picture to POSIX file \“/path/to/picture.x

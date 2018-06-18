c73d58e3f1f7c64a5ea03c6cddc46dc7f53f9470
yevbar <yevgeniybarkalov@yahoo.com>
2018-06-18 11:03:36 -0700

    Make script.py

---
+++ script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+"""
+desktop.sexy, inspired by rms.sexy
+
+To 
+"""
+
+# To space out the background changes
+from time import sleep
+from os import system
+
+counter = 0
+
+while True:
+    sleep(5)
+    system("feh --bg-fill ~/desktop.sexy/" + str(counter) + ".jpg")
+    counter += 1
+    if counter > 142:
+        counter = 0
